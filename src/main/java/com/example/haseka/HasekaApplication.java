2194c3add0cd26bf7df577c1db2a6c19d09258bc
IT25102353 <it25102353@my.sliit.lk>
2026-05-06 17:17:01 +0530

    initialization of hotel repository

---
+++ src/main/java/com/example/haseka/HasekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.haseka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HasekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HasekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/haseka/HasekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haseka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HasekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
